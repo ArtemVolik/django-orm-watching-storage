e75e9099b9f2bfef58d6281d2be85d3cec8405f8
artyom <art.volyk@gmail.com>
2020-11-17 18:49:08 +0200

    add environ settings DEBUG and SECRET_KEY

--- project/settings.py
+++ project/settings.py
@@ -17,9 +17,12 @@ DATABASES = {
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+env = Env()
+env.read_env()
 
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=False)
+
+SECRET_KEY = env.str("SECRET_KEY")
 
 ROOT_URLCONF = "project.urls"
 
