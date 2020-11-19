d12494d82837143465fcbaa275db3f44aee1484f
artyom <art.volyk@gmail.com>
2020-11-19 09:58:03 +0200

    remove database setting to .env replace dotenv with environ

--- project/settings.py
+++ project/settings.py
@@ -1,24 +1,21 @@
 import os
-from dotenv import load_dotenv
 from environs import Env
 
-load_dotenv()
+env = Env()
+env.read_env()
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'checkpoint.devman.org',
-        'PORT': '5434',
-        'NAME': 'checkpoint',
-        'USER': os.getenv('DATABASE_USER'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'ENGINE': env('DB_ENGINE'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
     }
 }
 
 INSTALLED_APPS = ['datacenter']
-
-env = Env()
-env.read_env()
 
 DEBUG = env.bool("DEBUG", default=False)
 
@@ -28,7 +25,6 @@ ROOT_URLCONF = "project.urls"
 
 ALLOWED_HOSTS = ['*']
 
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES = [
     {
@@ -37,7 +33,6 @@ TEMPLATES = [
         'APP_DIRS': True,
     },
 ]
-
 
 USE_L10N = True
 
